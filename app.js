cd59f2299f680d068fa2d9294245c01e7eff814b
Monica Bobadilla <monicabobadilla@monicabobadilla-macbookpro.local>
2023-08-04 09:48:43 -0400

    app.js is now tracked

---
+++ app.js
@@ -0,0 +1,14 @@
+function rgb(){
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+let letters = document.querySelectorAll('.letter')
+
+let colorChange = setInterval( function(){
+    for (let letter of letters){
+        letter.style.color = rgb();
+    }
+}, 1000);
